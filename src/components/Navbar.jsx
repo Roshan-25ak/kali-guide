f8056c13b5713fcdcdf993e697a5f5a076dce242
Roshan-25ak <roshan5747877@gmail.com>
2025-09-30 12:34:40 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="bg-gray-800 p-4 flex justify-between items-center">
-      <Link to="/" className="text-2xl font-bold">Welcome Hacker</Link>
+      <Link to="/" className="text-2xl font-bold">UDEMY COURSE FOR KALI LINUX</Link>
     </nav>
   );
 }
